02d9fe752fbf9cd45d20c511d41bb50d94d336ee
Miles Walker <mileswalker91@googlemail.com>
2018-06-13 21:56:15 +0100

    footer changes

--- wp-content/themes/firsttheme/footer.php
+++ wp-content/themes/firsttheme/footer.php
@@ -12,6 +12,17 @@
 </div>
 </div>
 
+<div class="footer container-fluid">
+  <div class="row">
+
+      <div class="col-md-2 footer-logo"><img src="http://wordpress.test/wp-content/uploads/2018/03/static1.squarespace-2.png" /></div>
+          <div class="col-md-2"><p>Property Devlopers</p></div>
+      <div class="col-md-2"><p>View Our <a href="#">Data Policy</a></p></div>
+      <div class="col-md-2"><p><a href="#">Contact us</a>now</p></div>
+      <div class="col-md-4 designed-by">Website Designed By <a href="">GITCRAKIN</a></div>
+
+  </div>
+</div>
 
 
 <?php wp_footer(); ?>
